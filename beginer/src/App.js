e32865d2e395ae35a10a771c80ea2c39dc82b3f4
Rainymemoryy <Rainy.memoryy@gmail.com>
2021-12-21 20:28:53 +0700

    clear project

--- beginer/src/App.js
+++ beginer/src/App.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-import style from './App.module.css'
-import User from './class_component/User'
-
-function App() {
-    return (
-        <div className={style.App}>
-            <User />
-        </div>
-    )
-=======
 function App() {
     return <div className='App'></div>
->>>>>>> parent of 1981c18 (clear)
 }
 
 export default App
