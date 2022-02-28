deb7f3f4497cc0a7ddf8534b719d33984d7678eb
Rainymemoryy <63650705+Rainymemoryy@users.noreply.github.com>
2022-02-28 21:25:57 +0700

    Revert "Revert "ProductDetail""
    This reverts commit 42e7e83bd75e105333116456504f19bd8c3fcb50.

--- shopee/src/untils/helper.js
+++ shopee/src/untils/helper.js
@@ -15,6 +15,11 @@ export const payloadCreator = asyncFunc => async (arg, thunkAPI) => {
 export const generateNameId = ({ name, _id }) =>
     encodeURIComponent(`${name.replace(/\s/g, '-').replace(/%/g, '')}-i.${_id}`)
 
+export const getIdFromNameId = url => {
+    const arr = url.split('-i.')
+    return arr[arr.length - 1]
+}
+
 export const formatMoney = (value, character = '.') => String(value).replace(/\B(?=(\d{3})+(?!\d))/g, character)
 
 export const formatK = value => {
@@ -24,3 +29,5 @@ export const formatK = value => {
     }
     return value
 }
+
+export const rateSale = (original, sale) => Math.round(((original - sale) / original) * 100) + '%'
